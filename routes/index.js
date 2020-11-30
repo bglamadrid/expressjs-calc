a1bb956fa517134ec74f9af37b1a05402ca0a9fe
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-11-30 15:42:50 -0300

    add form-based calculator logic

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,29 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express Calculator' });
+});
+router.post('/', function(req, res, next) {
+  let resultValue = 0;
+  const b = req.body;
+
+  if (b.operationType && b.operand1 && b.operand2) {
+    const operationTypes = {
+      'sum': '+',
+      'sub': '-',
+      'mul': '*',
+      'div': '/'
+    };
+    if (b.operationType in operationTypes) {
+      const op = operationTypes[b.operationType];
+      resultValue = eval(`${b.operand1} ${op} ${b.operand2}`);
+    }
+  }
+
+  res.render('index', {
+    title: 'Express Calculator',
+    resultValue
+  });
 });
 
 module.exports = router;
